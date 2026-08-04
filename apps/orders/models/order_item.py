28531184baa3ce99b57667652381b8d93bea6cdd
Komronbek Xusniddinov <komronbekx1@gmail.com>
2026-08-04 21:27:44 +0500

    add order, orderItem models

---
+++ apps/orders/models/order_item.py
@@ -0,0 +1,16 @@
+import uuid
+
+from apps.orders.models import Order
+from apps.products.models import Product
+from django.db import models
+
+
+class OrderItem(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="order_items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self) -> str:
+        return f"{self.product.name} x{self.quantity}"
